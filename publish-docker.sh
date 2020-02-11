1bdfcc92e5cb0665cad42a54859695c000c714b9
emech <mh.niroomand91@gmail.com>
2020-02-12 01:32:44 +0330

    Update publish bash script

--- publish-docker.sh
+++ publish-docker.sh
@@ -5,7 +5,7 @@ COMMIT_SSH=$(echo "$TRAVIS_COMMIT" | cut -c1-7)
 TAGS=()
 
 if [[ "$TRAVIS_BRANCH" = "develop" ]]; then
-  TAGS+=("$D_IMAGE_NAME:dev-lates $D_IMAGE_NAME:commit-$COMMIT_SSH")
+  TAGS+=("$D_IMAGE_NAME:dev-lates" "$D_IMAGE_NAME:commit-$COMMIT_SSH")
 elif [[ "$TRAVIS_BRANCH" = "master" ]]; then
   PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
   TAGS+=("$D_IMAGE_NAME:latest" "$D_IMAGE_NAME:$PACKAGE_VERSION" "$D_IMAGE_NAME:commit-$COMMIT_SSH")

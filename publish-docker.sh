c39aa42e1714eafecafeed0f570a9d770719883c
emech <mh.niroomand91@gmail.com>
2020-02-12 00:47:47 +0330

    Update deploy stage in .travis.yml

--- publish-docker.sh
+++ publish-docker.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-echo "\$1 = $1"
-if [[ "$1" = "" ]]; then
-  echo "EMPTY"
-  fi
-
 D_IMAGE_NAME=emech/simple-order
 COMMIT_SSH=$(echo "$TRAVIS_COMMIT" | cut -c1-7)
 TAGS=
 
-if [ "$1" = "develop" ]; then
+if [[ "$TRAVIS_BRANCH" = "develop" ]]; then
   TAGS="-t $D_IMAGE_NAME:dev-lates -t $D_IMAGE_NAME:dev-b$TRAVIS_BUILD_NUMBER-$COMMIT_SSH"
-elif [ "$1" = "master" ]; then
-  TAGS="-t $D_IMAGE_NAME:lates -t $D_IMAGE_NAME:$COMMIT_SSH"
-elif [ "$1" = "" ]; then
-  TAGS="-t $D_IMAGE_NAME:$TRAVIS_TAG"
+elif [[ "$TRAVIS_BRANCH" = "master" ]]; then
+  PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+  TAGS="-t $D_IMAGE_NAME:lates -t $D_IMAGE_NAME:$PACKAGE_VERSION"
 fi
 
 echo "------------------------------------"

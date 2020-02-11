60f8ce1dd0d0b9e61ad61076c1fc66ecef905e31
emech <mh.niroomand91@gmail.com>
2020-02-11 23:19:01 +0330

    UPDATE publish-docker.sh

--- publish-docker.sh
+++ publish-docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "$TRAVIS_BRANCH"
-echo "$TRAVIS_BUILD_NUMBER"
-echo "$TRAVIS_COMMIT"
-echo "$TRAVIS_BRANCH"
-echo "$TRAVIS_TAG"
+echo "Positional Parameters"
+echo "\$0 = $0"
+echo "\$1 = $1"
+echo "\$2 = $2"
+echo "\$3 = $3"
+echo "\$TRAVIS_BRANCH = $TRAVIS_BRANCH"
+echo "\$TRAVIS_BUILD_NUMBER = $TRAVIS_BUILD_NUMBER"
+echo "\$TRAVIS_COMMIT = $TRAVIS_COMMIT"
+echo "\$TRAVIS_TAG = $TRAVIS_TAG"

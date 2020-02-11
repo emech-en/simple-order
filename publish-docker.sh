09e93e01ae95aac297109c3b61274e52d0a61d55
emech <mh.niroomand91@gmail.com>
2020-02-11 23:38:50 +0330

    UPDATE publish-docker.sh

--- publish-docker.sh
+++ publish-docker.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-echo "Positional Parameters"
-echo "\$0 = $0"
-echo "\$1 = $1"
-echo "\$2 = $2"
-echo "\$3 = $3"
-echo "\$TRAVIS_BRANCH = $TRAVIS_BRANCH"
-echo "\$TRAVIS_BUILD_NUMBER = $TRAVIS_BUILD_NUMBER"
-echo "\$TRAVIS_COMMIT = $TRAVIS_COMMIT"
-echo "\$TRAVIS_TAG = $TRAVIS_TAG"
+D_IMAGE_NAME=emech/simple-order
+COMMIT_SSH=$(echo "$TRAVIS_COMMIT" | cut -c1-7)
+TAGS=
+
+if [ "$1" = "develop" ]; then
+  TAGS="-t $D_IMAGE_NAME:dev-lates -t $D_IMAGE_NAME:dev-b$TRAVIS_BUILD_NUMBER-$COMMIT_SSH"
+fi
+
+if [ "$1" = "master" ]; then
+  TAGS="-t $D_IMAGE_NAME:lates -t $D_IMAGE_NAME:$COMMIT_SSH"
+fi
+
+if [ "$1" = "tag" ]; then
+  TAGS="-t $D_IMAGE_NAME:$TRAVIS_TAG"
+fi
+
+docker build $TAGS .
+docker images ls
